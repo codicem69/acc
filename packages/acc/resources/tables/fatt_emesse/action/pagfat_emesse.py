0f577eb69b98d0f52a0802375636bb12076fbcad
tommaso <codicem69@gmail.com>
2023-09-11 15:43:05 +0200

    modifica delle action in fornitori e clienti per aggiornare il saldo con il multi pagamento

--- packages/acc/resources/tables/fatt_emesse/action/pagfat_emesse.py
+++ packages/acc/resources/tables/fatt_emesse/action/pagfat_emesse.py
@@ -3,6 +3,7 @@ from gnr.web.batch.btcaction import BaseResourceAction
 from decimal import Decimal
 from time import sleep
 import os 
+from gnr.core.gnrnumber import floatToDecimal,decimalRound
 
 caption = 'Saldo multiplo' #nome nel menu dei batch
 tags = 'admin,user'  #autorizzazione al batch
@@ -31,13 +32,36 @@ class Main(BaseResourceAction):
         iteratore_fatforn = self.btc.thermo_wrapper(records,message=self.messaggio_termometro, maximum=maximum) 
         
         #il metodo thermo_wrapper ottiene un iteratore che scorrendo ogni elemento aggiorna il termometro 
+        #con il ciclo for successivo aggiorniamo il pagamento delle fatture selezionate
         nuovo_pagcliente=None
         for record in iteratore_fatforn:
             saldo = record['saldo']
+            cliente_id = record['cliente_id']
             if saldo > 0:
                 fatcliente_id = record['id']
                 nuovo_pagcliente = self.db.table('acc.pag_fat_emesse').newrecord(fatt_emesse_id=fatcliente_id, data=data_saldo, importo=saldo, note=note)
                 self.db.table('acc.pag_fat_emesse').insert(nuovo_pagcliente)
+        #verifichiamo il totale delle fatture cliente poi preleviamo tutti gli id delle fatture cliente che con un ciclo for nella tbl 
+        # pagamenti cliente andremo a calcolare il totale pagato per poi calcolare il saldo
+        totale_fatture = self.db.table('acc.fatt_emesse').readColumns(columns="""SUM($importo) AS totale_fatture""",
+                                                                     where='$cliente_id=:c_id and $insda is not true',c_id=cliente_id)
+        fatture_cliente_id = self.db.table('acc.fatt_emesse').query(columns='$id',where='$cliente_id=:c_id', c_id=cliente_id).fetchAsDict('id')
+        totale_pagato = 0
+        for r in fatture_cliente_id:
+            pagamenti = self.db.table('acc.pag_fat_emesse').query(columns='$importo',
+                                                                where='$fatt_emesse_id=:fe_id', fe_id=r).fetch()
+            for a in range(len(pagamenti)):
+                totale_pagato += pagamenti[0][a]
+        #prendiamo il record della tabella cliente e con for_update=True successivamente faremo l'aggiornamento con il nuovo saldo 
+        tbl_cliente = self.db.table('acc.cliente')
+        record_cliente = tbl_cliente.record(where='$id=:id_cliente', 
+                                  id_cliente=cliente_id,
+                                  for_update=True).output('dict')
+        old_record = dict(record_cliente)
+        if totale_fatture is not None:
+            nuovo_record = dict(id=cliente_id,balance=floatToDecimal(totale_fatture - totale_pagato or 0))
+            tbl_cliente.update(nuovo_record,old_record)
+       
         if nuovo_pagcliente:
             self.db.commit()       
 
--- packages/acc/resources/tables/fatture_forn/action/pagforn.py
+++ packages/acc/resources/tables/fatture_forn/action/pagforn.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 from gnr.web.batch.btcaction import BaseResourceAction
 from decimal import Decimal
+from gnr.core.gnrnumber import floatToDecimal,decimalRound
 from time import sleep
 import os 
 
@@ -31,15 +32,39 @@ class Main(BaseResourceAction):
         iteratore_fatforn = self.btc.thermo_wrapper(records,message=self.messaggio_termometro, maximum=maximum) 
         
         #il metodo thermo_wrapper ottiene un iteratore che scorrendo ogni elemento aggiorna il termometro 
+        #con il ciclo for successivo aggiorniamo il pagamento delle fatture selezionate
         nuovo_pagforn=None
         for record in iteratore_fatforn:
             saldo = record['saldo']
             if saldo > 0:
                 fatforn_id = record['id']
+                fornitore_id = record['fornitore_id']
                 nuovo_pagforn = self.db.table('acc.pag_fat_forn').newrecord(fatture_forn_id=fatforn_id, data=data_saldo, importo=saldo, note=note)
                 self.db.table('acc.pag_fat_forn').insert(nuovo_pagforn)
+        #verifichiamo il totale delle fatture fornitore poi preleviamo tutti gli id delle fatture fornitori che con un ciclo for nella tbl 
+        # pagamenti fornitori andremo a calcolare il totale pagato per poi calcolare il saldo
+        totale_fatture = self.db.table('acc.fatture_forn').readColumns(columns="""SUM($importo) AS totale_fatture""",
+                                                                     where='$fornitore_id=:f_id',f_id=fornitore_id)
+        fatture_forn_id = self.db.table('acc.fatture_forn').query(columns='$id',where='$fornitore_id=:f_id', f_id=fornitore_id).fetchAsDict('id')
+        totale_pagato = 0
+        for r in fatture_forn_id:
+            pagamenti = self.db.table('acc.pag_fat_forn').query(columns='$importo',
+                                                                     where='$fatture_forn_id=:ff_id', ff_id=r).fetch()
+            for a in range(len(pagamenti)):
+                totale_pagato += pagamenti[a][0]
+        #prendiamo il record della tabella fornitore e con for_update=True successivamente faremo l'aggiornamento con il nuovo saldo 
+        tbl_fornitore = self.db.table('acc.fornitore')
+        record_fornitore = tbl_fornitore.record(where='$id=:id_forn', 
+                                  id_forn=fornitore_id,
+                                  for_update=True).output('dict')
+        old_record = dict(record_fornitore)
+           
+        if totale_fatture is not None:
+            nuovo_record = dict(id=fornitore_id,balance=floatToDecimal(totale_fatture - totale_pagato or 0))
+            tbl_fornitore.update(nuovo_record,old_record)
+
         if nuovo_pagforn:
-            self.db.commit()       
+            self.db.commit() 
 
     def messaggio_termometro(self,record, curr, tot, **kwargs):
         return "Invoice %s %i/%i" %(record['doc_n'],curr,tot)
